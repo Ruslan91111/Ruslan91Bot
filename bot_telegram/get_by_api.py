4c497ce277e2f0191b2f5f4bb4ab235fb9c2c62a
Ruslan91111 <ruslan_ru911@mail.ru>
2023-04-22 20:05:15 +0500

    fin get_weather_by_api

--- bot_telegram/get_by_api.py
+++ bot_telegram/get_by_api.py
@@ -5,18 +5,21 @@ from config import API_WEATHER
 
 
 async def get_the_weather_by_api(city: str) -> str:
-    url = f"http://api.openweathermap.org/data/2.5/find?q={city.strip()}&type=like&APPID={API_WEATHER}"
+    """ Запрашивает через API данные погоды в определенном городе, возвращает готовый ответ в виде строки."""
+    url = f"http://api.openweathermap.org/data/2.5/find?q={city.strip()}&type=like&APPID={API_WEATHER}&units=metric"
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             response_text = await response.text()
-            print(response_text)
+            # Преобразовываем в словарь.
+            weather_in_city = json.loads(response_text)
+            # Выбираем нужные нам значения, из которых сформируем возвращаемую строку.
+            real_temperature = weather_in_city['list'][0]['main']['temp']
+            fills_like_temperature = weather_in_city['list'][0]['main']['feels_like']
+            wind = weather_in_city['list'][0]['wind']['speed']
+            rain = ', в настоящее время идёт дождь' if weather_in_city['list'][0]['rain'] else ''
+            snow = ', в настоящее время идёт снег' if weather_in_city['list'][0]['snow'] else ''
+
+            return(f'В настоящее время в населенном пункте {city} температура воздуха {real_temperature}°C, '
+                   f'ощущается как {fills_like_temperature}°C, скорость ветра {wind}м/с{rain}{snow}')
 
 
-
-# Вариант через requests
-# def get_the_weather_by_api(city: str) -> str:
-#     url = f"http://api.openweathermap.org/data/2.5/find?q={city.strip()}&type=like&APPID=8537d9ef6386cb97156fd47d832f479c"
-#     print(url)
-#     response = requests.get(url)
-#     x = response.json()
-#     print(x)
